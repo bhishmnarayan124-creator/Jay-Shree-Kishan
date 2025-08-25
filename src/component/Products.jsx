eedd73b17efe39ae749d101c9c29b4598c670431
Bhishm Narayan <bhishmnarayan124@gmail.com>
2025-08-25 15:07:27 +0530

    h

---
+++ src/component/Products.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Products.css'
+
+const Products = () => {
+  return (
+    <div className='product-container'>
+      <div>
+        <p className=''>Top Product</p>
+      <p className='mt-16 text-2xl md:text-3xl font-medium'>Do not miss the current offers until the end of March.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Products
