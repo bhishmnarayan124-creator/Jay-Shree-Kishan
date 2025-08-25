e499a0cc732b4ee045a2b7b1cb27c0c9c695be9e
Bhishm Narayan <bhishmnarayan124@gmail.com>
2025-08-25 15:01:30 +0530

    hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Products from './component/Products'
+import Home from './component/Home'
 
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <Home/>
     </div>
   )
 }
