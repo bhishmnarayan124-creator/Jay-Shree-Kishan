5da33f826014e7346984d4a27ca93a66d547ba75
Nitesh yadav <nitesh000raoyadav@gmail.com>
2025-08-25 22:31:15 +0530

    nitesh

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,12 +3,19 @@ import slider from './imgi_34_3-1000x700.jpg'
 import slider_1 from './imgi_35_4-1000x700.jpg'
 import slider_2 from './imgi_36_5-1000x700.jpg'
 import slider_3 from './imgi_37_1-1000x700.jpg'
-
+import banner from './imgi_12_1-1350x100.jpg'
+import banner1 from './imgi_13_1-430x300.jpg'
+import banner2 from './imgi_14_2-430x300.jpg'
+import banner3 from './imgi_15_3-430x300.jpg'
 
 export const assets = {
     logo,
     slider,
     slider_1,
     slider_2,
-    slider_3
+    slider_3,
+    banner,
+    banner1,
+    banner2,
+    banner3
 }
