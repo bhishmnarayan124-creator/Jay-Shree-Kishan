af82561d0f5786f11a552d7378e6da6a63055e69
padam saini <sainipadam373@gmail.com>
2025-08-28 23:53:12 -0700

    padam

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -9,34 +9,23 @@ import Category from './category/Category'
 
 
 import Search from './Searchbar/Search'
-<<<<<<< HEAD
+
 
 
 import Products from './Products'
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 // import BuySellForm from './CategoryData/BuySellForm'
 import TrendingPrices from './CategoryData/TrendingPrices'
 // import EggRateTable from './CategoryData/EggRateTable'
-=======
-=======
 
 
-import TrendingPrices from './CategoryData/TrendingPrices'
 
 
->>>>>>> f503a2213764f0a2b8542a9e38cf407acad343a8
+
 
 import BuySellForm from './CategoryData/BuySellForm'
-=======
+
 import Collcard from './CategoryData/Collcard'
->>>>>>> a5f44b9c5f42f20257e4d52004d886647feeb9fa
-
-
-
-
-
-import TrendingPrices from './CategoryData/TrendingPrices'
 
 
 
@@ -49,11 +38,12 @@ import TrendingPrices from './CategoryData/TrendingPrices'
 
 
 
-<<<<<<< HEAD
->>>>>>> ce2b5017b5d7f40df20bc1c4b97a074f150336bb
-=======
 
->>>>>>> f503a2213764f0a2b8542a9e38cf407acad343a8
+
+
+
+
+
 
 const Home = () => {
   return (
@@ -68,24 +58,20 @@ const Home = () => {
       <Banner/>
      
       <BlogSection/>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
       {/* <BuySellForm/> */}
        <TrendingPrices/>
        {/* <EggRateTable/> */}
-=======
-=======
->>>>>>> f503a2213764f0a2b8542a9e38cf407acad343a8
+
 
       <BuySellForm/>
        <TrendingPrices/>
-       <EggRateTable/>
-=======
+       {/* <EggRateTable/> */}
+
       <Collcard/>
       
        
->>>>>>> a5f44b9c5f42f20257e4d52004d886647feeb9fa
+
 
       {/* <BuySellForm/> */}
 
@@ -99,16 +85,7 @@ const Home = () => {
        {/* <TrendingPrices/> */}
        {/* <EggRateTable/> */}
 
-      
-
-<<<<<<< HEAD
->>>>>>> ce2b5017b5d7f40df20bc1c4b97a074f150336bb
-      
-=======
-
-     
->>>>>>> f503a2213764f0a2b8542a9e38cf407acad343a8
-      
+ 
       
 
     </div>
