c7c3167e7d6eeabbe906e5e4d2ba3def3b465f1e
murtidip <dipanshusharma5334@gmail.com>
2025-09-02 13:29:35 +0530

    dipanshu

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,62 +1,18 @@
 import React from 'react'
 import BlogSection from './BlogSection'
-
 import Banner from './banner/Banner'
-
-
-
-
-
 import Search from './Searchbar/Search'
 import Collcard from './CategoryData/Collcard'
 import Features from './category/Category'
 
 
-
-
-
-
-import TrendingPrices from './CategoryData/TrendingPrices'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const Home = () => {
   return (
     <div>
-
-    
       <Features/>
       <Search/>
-      
-      
       <Banner/>
-     
       <BlogSection/>
-     
-       
-
-
-
-    
-
-      
-
-
-     
-      
-      
-
     </div>
   )
 }
