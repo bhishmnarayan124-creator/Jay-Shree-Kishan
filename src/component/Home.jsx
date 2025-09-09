9764bcc560d3fccafb2f7b0c2df8b0e0b7c413c6
murtidip <dipanshusharma5334@gmail.com>
2025-09-09 13:34:16 +0530

    dipanshu

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -2,31 +2,19 @@ import React from 'react'
 import BlogSection from './BlogSection'
 import Banner from './banner/Banner'
 
-
-
-
-
-
-
-
-
-
-
 import Search from './Searchbar/Search'
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 2278f366e1023461aae239fee1333a499c8d2521
 import Features from './category/Category'
+import ProductList from './CategoryData/ProductList'
 
 
 
 
 
 
-import TrendingPrices from './CategoryData/TrendingPrices'
-import Special from './special/Special'
+
+
 
 
 
@@ -47,12 +35,12 @@ const Home = () => {
     <div>
       <Features/>
       <Search/>
+       <ProductList/>
+       
       <Banner/>
       <BlogSection/>
-<<<<<<< HEAD
-=======
 
->>>>>>> 329bff748bd1fb861c94151718104bf2663d9b2e
+     
     </div>
   )
 }
--- src/component/Searchbar/Search.jsx
+++ src/component/Searchbar/Search.jsx
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Search.css'
 import { FiSearch } from "react-icons/fi";
-const Search = () => {
-  return (
-    <>
-    <div className='srch'>
-      <input type="text" placeholder='search' />
-      <FiSearch  className='icn'/>
-      
-    </div>
 
-    </>    
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+
+  const handleSearch = async (e) => {
+    const value = e.target.value;
+    setQuery(value);
+
+    if (value.trim() !== "") {
+      const res = await fetch(`http://localhost:5000/api/search?q=${value}`);
+      const data = await res.json();
+      setResults(data);
+    } else {
+      setResults([]);
+    }
+  };
+
+  // ✅ Suggestion select karne par input me set karo
+  const handleSelect = (name) => {
+    setQuery();     // input box update
+    setResults([]);     // list hide
+  };
+
+  return (
+    <div className='srch'>
+      <input
+        type="text"
+        value={query}
+        onChange={handleSearch}
+        placeholder='search'
+      />
+      <FiSearch className='icn'/>
+
+      {results.length > 0 && (
+        <div className="results">
+          {results.map((item) => (
+            <p key={item._id} onClick={() => handleSelect(item.name)}>
+              {item.name}
+            </p>
+          ))}
+        </div>
+      )}
+    </div>
   )
 }
 
